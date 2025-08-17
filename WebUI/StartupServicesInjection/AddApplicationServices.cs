a608f64545d63bafa7bc83bb2071334a97195f50
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-08-17 09:35:01 +0300

    Initial commit

---
+++ WebUI/StartupServicesInjection/AddApplicationServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace WebUI.StartupServicesInjection
+{
+    public static class AddApplicationServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+            return services;
+        }
+    }
+}
